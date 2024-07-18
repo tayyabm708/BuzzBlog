dfb2b75d74f9fcdc530eb6ffba4a10d732ce6da9
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-18 21:22:15 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import { mongoose } from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
 const app = express();
 
