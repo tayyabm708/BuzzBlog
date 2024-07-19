07106f78a0ff86f1f0b0b836ceec746ba7110856
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-19 19:36:45 +0500

    complete sigin page func

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    res.status(200).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
   }
