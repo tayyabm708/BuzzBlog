48c723804f790084dc28eeedd4fe25afb4aa4ad9
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-20 16:29:04 +0500

    add sidebar to dasboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../compnents/DashSidebar";
+import DashProfile from "../compnents/DashProfile";
 
 function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab ] = useState('');
+  useEffect(() => {
+    const urlPrams = new URLSearchParams(location.search);
+    const tabFromUrl = urlPrams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/*Sidebar */}
+        <DashSidebar />
+      </div>
+      {/*Profile...*/}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
