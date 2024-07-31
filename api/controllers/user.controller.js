b5801157484cec870c37335cc57fceb44fe4338b
Muhammad Tayyab <tayyabm708@gmail.com>
2024-07-31 16:29:07 +0500

    create delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,6 @@
-
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-import errorHandler from "../utils/error.js"
+import errorHandler from "../utils/error.js";
 export const test = (req, res) => {
   res.json({ message: "Test Api is  working" });
 };
@@ -17,38 +16,53 @@ export const updateUser = async (req, res, next) => {
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
 
-  
-  if(req.body.username){
-    if(req.body.username.length < 7 || req.body.username.length > 20){
-        return next(errorHandler(400, "Username must be between 7 and 20 characters"));
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
     }
-    if(req.body.username.includes(' ')){
-        return next(errorHandler(400, "Username must not contain spaces"));
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username must not contain spaces"));
     }
-    if(req.body.username !== req.body.username.toLowerCase()){
-        return next(errorHandler(400, "Username must be lowercase"));
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
     }
-    if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
-        return next(errorHandler(400, "Username must contain only letters and numbers"));
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username must contain only letters and numbers")
+      );
     }
-  
   }
-  
-    try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.userId, 
-            {
-                $set: {
-                    username: req.body.username,
-                    email: req.body.email,
-                    password: req.body.password,
-                    profilePicture: req.body.profilePicture,
-                },
-            }, 
-            { new: true });
-        const { password, ...rest} = updatedUser._doc;
-        res.status(200).json(rest);
-    } 
-    catch (error) {
-        next(error);
-    }
-  };
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "User has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
