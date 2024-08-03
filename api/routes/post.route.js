6e78c2b91de2554e46096bcb956ce38abbe9b3ed
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-03 12:40:53 +0500

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js"
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 
 
-router.post("/create", verifyToken, create)
+router.post("/create", verifyToken, create);
+router.get("/getposts", getPosts);
+
 
 
 export default router;
