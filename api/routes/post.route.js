d05601a5f0974df9c2090195dda68fc110741627
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-03 13:57:01 +0500

    add delete post fnc

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { verifyToken } from "../utils/verifyUser.js"
-import { create, getPosts } from "../controllers/post.controller.js";
+import { create, getPosts, deletePost } from "../controllers/post.controller.js";
 
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
 
 
 
