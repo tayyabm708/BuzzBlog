1143a40c782249600d937293767cf4a1eac53f3e
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-07 22:30:58 +0500

    Add recent articles to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -4,8 +4,10 @@ import { Link, useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
 import CallToAction from "../compnents/CallToAction";
 import CommentsSection from "../compnents/CommentsSection";
+import PostCard from "../compnents/PostCard";
 
 export default function PostPage() {
+  const [recentPosts, setRecentPosts] = useState(null);
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
@@ -33,6 +35,23 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
+  
 
   if (loading)
     return (
@@ -69,14 +88,21 @@ export default function PostPage() {
       <div
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
-      >
-      </div>
+      ></div>
 
       <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
       </div>
 
       <CommentsSection postId={post._id} />
+
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
