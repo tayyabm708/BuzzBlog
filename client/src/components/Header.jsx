4b8e1fbc8a3041ae2180176ff6327f6f269325cd
Muhammad Tayyab <tayyabm708@gmail.com>
2024-08-08 15:45:54 +0500

    fixes some issue in header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -72,7 +72,7 @@ export default function Header() {
     
       <div className='flex gap-2 md:order-2'>
         <Button
-          className='w-12 h-10 hidden sm:inline'
+          className='w-12 h-10 sm:inline'
           color='gray'
           pill
           onClick={() => dispatch(toggleTheme())}
